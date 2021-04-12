9e5a77b1667a19760f9209ce68dee8939ab04274
Corey Murphy-Vixie <corey@vixie.enterprises>
2021-04-11 17:38:20 -0700

    Fix test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ export default {
   testEnvironment: "node",
   preset: "ts-jest/presets/js-with-ts",
   transformIgnorePatterns: ["/node_modules/(?!(ky))"],
+  testPathIgnorePatterns: ["/test/stubs/"],
+  collectCoverageFrom: ["src/**"],
 };
