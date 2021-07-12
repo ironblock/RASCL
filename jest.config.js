9727de17a5b1178c1d3eeb1fe5abce4525273a14
Corey Murphy-Vixie <corey@vixie.enterprises>
2021-07-12 10:51:14 -0700

    Fix tests to be more robust in the face of ESM

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 export default {
-  testEnvironment: "node",
-  preset: "ts-jest/presets/js-with-ts",
-  transformIgnorePatterns: ["/node_modules/(?!(ky))"],
-  testPathIgnorePatterns: ["/test/stubs/"],
+  preset: "ts-jest/presets/js-with-ts-esm",
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  extensionsToTreatAsEsm: [".ts"],
   collectCoverageFrom: ["src/**"],
+  transformIgnorePatterns: ["node_modules/(?!(ky))"],
 };
